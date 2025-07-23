15f5dacbdf8dd3f1b1c9d9a0b251691326d1472d
Sönke Ludwig <sludwig@outerproduct.org>
2025-07-23 10:53:48 +0200

    Add compatibility module.

---
+++ source/vibe/container/vector.d
@@ -0,0 +1,7 @@
+deprecated("Use vibe.container.array.Array instead")
+module vibe.container.vector;
+
+public import vibe.container.array;
+
+deprecated("Use vibe.container.array.Array instead")
+alias Vector = Array;
